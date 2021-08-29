f38216349f9c1c3f28034a4c62076691817882b1
NaP <nahuel2998@gmail.com>
2021-08-29 02:16:37 -0300

    A2: Implement fixes and improvements to Menu

--- ProgIIProjects-UCU/NarLib/Menu.cs
+++ ProgIIProjects-UCU/NarLib/Menu.cs
@@ -1,49 +1,94 @@
 using System;
-using System.Collections.Generic;
 
-static void BuildMenu(List<Option> options)
+namespace NarLib
 {
-    ConsoleKeyInfo key;
-    int index = 0;
-
-    while (true)
+    public class Menu
     {
-        switch (Console.ReadKey())
+        #region BuildMenu
+        // Build Menu with Exit Option
+        static void BuildMenu(string title, Option[] options, string exitText)
         {
-            case Consolekey.DownArrow:
-                if (index + 1 < options.Count)
-                    RenderMenu(options, options[++index])
-                break;
-            case Consolekey.UpArrow:
-                if (index > 0)
-                    RenderMenu(options, options[--index])
-                break;
-            case ConsoleKey.Enter:
-                options[index].Selected.Invoke();
-                index = 0;
-                break;
+            int index = 0;
+            int exitVal = options.Length;
+
+            while (true)
+            {
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        RenderMenu(options, index + 1 < exitVal ? ++index : exitVal, exitText);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        RenderMenu(options, index > 0 ? --index : exitVal, exitText);
+                        break;
+                    case ConsoleKey.RightArrow: 
+                    case ConsoleKey.Enter:
+                        if (index == exitVal)
+                            return;
+                        options[index].Selected.Invoke();
+                        RenderMenu(options, index, exitText);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        return;
+                }
+            }
         }
+
+        // Build Menu without Exit Option
+        static void BuildMenu(string title, Option[] options)
+        {
+            int index = 0;
+
+            while (true)
+            {
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        RenderMenu(options, index + 1 < options.Length ? ++index : options.Length);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        RenderMenu(options, index > 0 ? --index : options.Length);
+                        break;
+                    case ConsoleKey.RightArrow: 
+                    case ConsoleKey.Enter:
+                        options[index].Selected.Invoke();
+                        RenderMenu(options, index);
+                        break;
+                }
+            }
+        }
+        #endregion
+
+        #region RenderMenu
+        // Render Menu with Exit Option
+        static void RenderMenu(Option[] options, int selectedIndex, string exitText)
+        {
+            Console.Clear();
+            for (int i = 0; i < options.Length; i++)
+                Console.WriteLine($"{(i == selectedIndex ? "->" : "")}\t{options[i].Name}");
+            Console.WriteLine($"{(selectedIndex == options.Length ? "->" : "")}\t{exitText}");
+        }
+
+        // Render Menu without Exit Option
+        static void RenderMenu(Option[] options, int selectedIndex)
+        {
+            Console.Clear();
+            for (int i = 0; i < options.Length; i++)
+                Console.WriteLine($"{(i == selectedIndex ? "->" : "")}\t{options[i].Name}");
+        }
+        #endregion
     }
-}
 
-static void RenderMenu(List<Option> options, Option selectedOption)
-{
-    Console.Clear();
-
-    foreach (o in options)
-        Console.WriteLine($"{o == selectedOption ? '>' : ''} {option.Name}");
-}
-
-class Option
-{
-    public string Name { get; }
-    public Action Selected { get; }
-    public ConsoleKeyInfo Shortcut { get; }
-
-    public Option(string name, Action selected, ConsoleKeyInfo shortcut = null)
+    class Option
     {
-        Name = name;
-        Selected = selected; 
-        Shortcut = shortcut;
+        public string Name { get; }
+        public Action Selected { get; }
+        public ConsoleKeyInfo Shortcut { get; }
+    
+        public Option(string name, Action selected)
+        {
+            Name = name;
+            Selected = selected;
+        }
     }
 }
