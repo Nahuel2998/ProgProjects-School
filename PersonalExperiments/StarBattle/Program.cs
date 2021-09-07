b0cc080e914be990f9cda4be0201e9c067e7bd7a
NaP <Nahuel2998@gmail.com>
2021-09-07 02:29:49 -0300

    A6: Now supporting any board dimension [Star Battle]

--- PersonalExperiments/StarBattle/Program.cs
+++ PersonalExperiments/StarBattle/Program.cs
@@ -32,38 +32,36 @@ namespace StarBattle
 
         static string GenerateBoardBoarders(char[,] board)
         {
-            char[,] res = new char[,]
-            {
-                {'┌', '─', '─', '─', '─', '─', '─', '─', '┐'},
-                {'│', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '│'},
-                {'│', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '│'},
-                {'│', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '│'},
-                {'│', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '│'},
-                {'│', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '│'},
-                {'│', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '│'},
-                {'│', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '│'},
-                {'└', '─', '─', '─', '─', '─', '─', '─', '┘'}
-            };
-            
             // board's bottom corner values
             int maxI = board.GetLength(0) - 1;
             int maxJ = board.GetLength(1) - 1;
 
+            // Generating res Canvas
+            char[,] res = new char[(maxI + 1)*2 + 1, (maxJ + 1)*2 + 1];
+
             // Generate Vertical Splits
             for (int i = 0; i <= maxI; i++)
-                for (int j = 1; j <= maxJ; j++) 
+                for (int j = 1; j <= maxJ; j++)
                     if (board[i, j] != board[i, j - 1])
                         res[i*2 + 1, j*2] = '│';
 
             // Generate Horizontal Splits
             for (int j = 0; j <= maxJ; j++)
-                for (int i = 1; i <= maxI; i++) 
+                for (int i = 1; i <= maxI; i++)
                     if (board[i, j] != board[i - 1, j])
                         res[i*2, j*2 + 1] = '─';
 
             // Switching to res' values
             maxI = res.GetLength(0) - 1;
             maxJ = res.GetLength(1) - 1;
+
+            // Generate Vertical Borders
+            for (int i = 0; i <= maxI; i++)
+                res[i, 0] = res[i, maxJ] = '│';
+
+            // Generate Horizontal Borders
+            for (int j = 0; j <= maxJ; j++)
+                res[0, j] = res[maxI, j] = '─';
 
             // Generate Corners
             for (int i = 0; i <= maxI; i += 2)
@@ -72,16 +70,16 @@ namespace StarBattle
                     int cornerVal = 0;
 
                     if (i != 0)
-                        if (!res[i - 1, j].Equals(' ')) 
+                        if (!res[i - 1, j].Equals('\0'))
                             cornerVal += UP;
                     if (j != 0)
-                        if (!res[i, j - 1].Equals(' ')) 
+                        if (!res[i, j - 1].Equals('\0'))
                             cornerVal += LEFT;
                     if (i != maxI)
-                        if (!res[i + 1, j].Equals(' ')) 
+                        if (!res[i + 1, j].Equals('\0'))
                             cornerVal += DOWN;
                     if (j != maxJ)
-                        if (!res[i, j + 1].Equals(' ')) 
+                        if (!res[i, j + 1].Equals('\0'))
                             cornerVal += RIGHT;
 
                     res[i, j] = getCorner[cornerVal];
@@ -91,7 +89,7 @@ namespace StarBattle
             for (int i = 0; i <= maxI; i++)
             {
                 for (int j = 0; j <= maxJ; j++)
-                    realres.Append(res[i, j]);
+                    realres.Append(res[i, j].Equals('\0') ? ' ' : res[i, j]);
                 realres.Append("\n");
             }
 
@@ -106,10 +104,11 @@ namespace StarBattle
             // yeah
             char[,] board = new char[,]
             {
-                {'A', 'B', 'C', 'D'},
-                {'A', 'B', 'C', 'C'},
-                {'A', 'C', 'C', 'B'},
-                {'A', 'A', 'C', 'B'}
+                {'A', 'B', 'C', 'D', 'D'},
+                {'A', 'B', 'C', 'C', 'D'},
+                {'A', 'C', 'C', 'B', 'D'},
+                {'A', 'A', 'C', 'B', 'D'},
+                {'A', 'B', 'B', 'B', 'B'}
             };
             string thething = GenerateBoardBoarders(board);
             Console.WriteLine(thething);
