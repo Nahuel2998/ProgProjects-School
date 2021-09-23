db8ee01b9c2cf50b522e64bf5b9f7dafffcf1182
NaP <nahuel2998@gmail.com>
2021-09-23 03:50:41 -0300

    A11: GetValidInput.cs created [NarLib]

---
+++ libs/NarLib/GetValidInput.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace NarLib
+{
+    public class GetValidInput
+    {
+        static string defaultError = "Valor no valido. Intentelo nuevamente.";
+
+        static int GetValidIntInput(string prompt, string splitArg = " ", bool clear = true, string parseError = null, Func<int, bool> conditionFunc = null, string conditionError = null)
+        {
+            while (true)
+            {
+                int res;
+                if (clear) { Console.Clear(); }
+                Console.WriteLine(prompt);
+                Console.Write("> ");
+                string input = Console.ReadLine().Split(splitArg)[0];
+                if (Int32.TryParse(input, out res))
+                {
+                    if (conditionFunc != null)
+                    {
+                        if (conditionFunc(res)) { return res; }
+
+                        Console.WriteLine(String.IsNullOrEmpty(conditionError) ? defaultError : conditionError);
+                        Console.ReadLine();
+                        continue;
+                    }
+                    return res;
+                }
+                Console.WriteLine(String.IsNullOrEmpty(parseError) ? defaultError : parseError);
+                Console.ReadLine();
+            }
+        }
+
+        static double GetValidDoubleInput(string prompt, string splitArg = " ", bool clear = true, string parseError = null, Func<double, bool> conditionFunc = null, string conditionError = null)
+        {
+            while (true)
+            {
+                double res;
+                if (clear) { Console.Clear(); }
+                Console.WriteLine(prompt);
+                Console.Write("> ");
+                string input = Console.ReadLine().Split(splitArg)[0];
+                if (Double.TryParse(input, out res))
+                {
+                    if (conditionFunc != null)
+                    {
+                        if (conditionFunc(res)) { return res; }
+
+                        Console.WriteLine(String.IsNullOrEmpty(conditionError) ? defaultError : conditionError);
+                        Console.ReadLine();
+                        continue;
+                    }
+                    return res;
+                }
+                Console.WriteLine(String.IsNullOrEmpty(parseError) ? defaultError : parseError);
+                Console.ReadLine();
+            }
+        }
+    }
+}
