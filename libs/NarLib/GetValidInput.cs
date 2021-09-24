cd9d2e4bde0afbec7c295e78f1540b9dd18b80ef
NaP <nahuel2998@gmail.com>
2021-09-24 03:25:51 -0300

    A12: Small fixes and two more functions.

--- libs/NarLib/GetValidInput.cs
+++ libs/NarLib/GetValidInput.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace NarLib
 {
@@ -6,15 +7,13 @@ namespace NarLib
     {
         private static string defaultError = "Valor no valido. Intentelo nuevamente.";
 
-        public static int GetValidIntInput(string prompt, string splitArg = " ", bool clear = true, string parseError = null, Func<int, bool> conditionFunc = null, string conditionError = null)
+        public static int GetValidIntInput(string prompt, string cursor = "> ", string splitArg = " ", bool clear = true, string parseError = null, Func<int, bool> conditionFunc = null, string conditionError = null)
         {
+            int res;
             while (true)
             {
-                int res;
                 if (clear) { Console.Clear(); }
-                Console.WriteLine(prompt);
-                Console.Write("> ");
-                string input = Console.ReadLine().Split(splitArg)[0];
+                string input = GetInput(prompt, cursor).Split(splitArg)[0];
                 if (Int32.TryParse(input, out res))
                 {
                     if (conditionFunc != null)
@@ -32,15 +31,13 @@ namespace NarLib
             }
         }
 
-        public static double GetValidDoubleInput(string prompt, string splitArg = " ", bool clear = true, string parseError = null, Func<double, bool> conditionFunc = null, string conditionError = null)
+        public static double GetValidDoubleInput(string prompt, string cursor = "> ", string splitArg = " ", bool clear = true, string parseError = null, Func<double, bool> conditionFunc = null, string conditionError = null)
         {
+            double res;
             while (true)
             {
-                double res;
                 if (clear) { Console.Clear(); }
-                Console.WriteLine(prompt);
-                Console.Write("> ");
-                string input = Console.ReadLine().Split(splitArg)[0];
+                string input = GetInput(prompt, cursor).Split(splitArg)[0];
                 if (Double.TryParse(input, out res))
                 {
                     if (conditionFunc != null)
@@ -56,6 +53,31 @@ namespace NarLib
                 Console.WriteLine(String.IsNullOrEmpty(parseError) ? defaultError : parseError);
                 Console.ReadLine();
             }
+        }
+
+        public static string GetValidStringInput(string prompt, string cursor = "> ", string splitArg = " ", bool clear = true, string conditionRegex = null, string conditionError = null, bool ignoreCase = false)
+        {
+            string res;
+            while (true)
+            {
+                if (clear) { Console.Clear(); }
+                res = GetInput(prompt, cursor).Split(splitArg)[0];
+                if (conditionRegex != null) 
+                { 
+                    if (Regex.IsMatch(res, conditionRegex, ignoreCase ? RegexOptions.IgnoreCase : RegexOptions.None)) { return res; }
+                    Console.WriteLine(String.IsNullOrEmpty(conditionError) ? defaultError : conditionError);
+                    Console.ReadLine();
+                    continue;
+                }
+                return res; 
+            }
+        }
+
+        public static string GetInput(string prompt = null, string cursor = "> ")
+        {
+            if (!String.IsNullOrEmpty(prompt)) { Console.WriteLine(prompt); }
+            if (!String.IsNullOrEmpty(cursor)) { Console.Write(cursor); }
+            return Console.ReadLine();
         }
     }
 }
