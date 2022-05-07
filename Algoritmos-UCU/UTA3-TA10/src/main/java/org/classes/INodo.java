bac3f661a68701488a29b2cfec84cb651ebee9f6
NaP <nahuel2998@gmail.com>
2022-05-07 06:48:32 -0300

    TODO: Become Generic.

---
+++ Algoritmos-UCU/UTA3-TA10/src/main/java/org/classes/INodo.java
@@ -0,0 +1,36 @@
+package org.classes;
+
+//interface INodo
+//{
+//    float getPrecio();
+//    void setDato(Object dato);
+//    void setSiguiente(INodo nodo);
+//    INodo getSiguiente();
+//    void imprimir();
+//    void imprimirEtiqueta();
+//    boolean equals(INodo unNodo);
+//    Comparable getEtiqueta();
+//    int compareTo(Comparable etiqueta);
+//}
+
+
+interface INodo
+{
+    float getPrecio();
+    void setPrecio(float precio);
+    String getId();
+    String getNombre();
+    void setNombre(String nombre);
+    void setSiguiente(INodo nodo);
+    INodo getSiguiente();
+    INodo clonar();
+    String imprimir(int labels, String separador);
+    String imprimir(int labels);
+    String imprimir();
+
+    // @Override
+    boolean equals(INodo nodo);
+
+//    // @Override
+//    int compareTo(Comparable etiqueta);
+}
