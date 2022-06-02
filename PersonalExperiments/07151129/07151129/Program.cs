7bb7c465dccc2ca15baa78b61860030b796986bd
NaP <nahuel2998@gmail.com>
2022-06-02 18:06:15 -0300

    New Project: 07151129

---
+++ PersonalExperiments/07151129/07151129/Program.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+
+namespace _07151129
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser parser = new(with =>
+            {
+                with.IgnoreUnknownArguments = true;
+                with.AutoHelp = false;
+                with.CaseSensitive = false;
+            });
+            OptionsHolder.Instance.Options = parser.ParseArguments<Options>(args).Value;
+        }
+    }
+}
+
