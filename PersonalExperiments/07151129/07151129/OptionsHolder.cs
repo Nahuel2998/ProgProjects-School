709c43b32f7ebd4cc2a88297b48542a57fde5f9f
NaP <nahuel2998@gmail.com>
2022-06-02 22:22:20 -0300

    Args

--- PersonalExperiments/07151129/07151129/OptionsHolder.cs
+++ PersonalExperiments/07151129/07151129/OptionsHolder.cs
@@ -1,13 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommandLine;
 
 namespace _07151129
 {
-    internal class OptionsHolder
+    public sealed class OptionsHolder
     {
+        public Options? Options { get; set; }
 
+        private static readonly OptionsHolder instance = new();
+
+        static OptionsHolder() { }
+        private OptionsHolder() { }
+
+        public static OptionsHolder Instance
+        { get { return instance; } }
+    }
+
+    public class Options
+    {
+        [Option('h', "help", HelpText="Help.")]
+        public bool Help { get; set; }
+
+        [Option("please", HelpText="Please use please.")]
+        public bool Please { get; set; }
+
+        [Option('c', "culprit", HelpText="The Culprit is Hideyoshi eating a donut awkwardly slowly.")]
+        public string? Culprit { get; set; }
     }
 }
+
--- PersonalExperiments/07151129/07151129/Program.cs
+++ PersonalExperiments/07151129/07151129/Program.cs
@@ -12,7 +12,28 @@ namespace _07151129
                 with.AutoHelp = false;
                 with.CaseSensitive = false;
             });
+
             OptionsHolder.Instance.Options = parser.ParseArguments<Options>(args).Value;
+
+            if (!OptionsHolder.Instance.Options.Please)
+            {
+                Console.WriteLine("no");
+                Console.ReadKey();
+                return;
+            }
+
+            if (OptionsHolder.Instance.Options.Help)
+            {
+                Console.WriteLine(string.Join(' ', args));
+                return;
+            }
+
+            Console.WriteLine(OptionsHolder.Instance.Options.Culprit ?? "Hola.");
+
+            // Microsoft.Win32.RegistryKey key;
+            // key = Microsoft.Win32.Registry.CurrentUser.CreateSubKey("YOSANKAEVA").CreateSubKey("07151129");
+            // key.SetValue("Culprit", OptionsHolder.Instance.Options.Culprit);
+            // key.Close();
         }
     }
 }
