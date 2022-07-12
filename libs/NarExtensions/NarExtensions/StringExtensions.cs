4c4c97ea97c6c5704aff3ec9b1952d96d5f1a4cf
Nar <Nahuel2998@gmail.com>
2022-07-12 08:55:42 -0300

    Support padding multiline strings horizontally

--- libs/NarExtensions/NarExtensions/StringExtensions.cs
+++ libs/NarExtensions/NarExtensions/StringExtensions.cs
@@ -7,14 +7,14 @@ namespace NarExtensions
     {
         public static string Multiply(this string source, int multiplier)
         {
-            StringBuilder res = new(multiplier * source.Length);
+            StringBuilder res = new(Math.Max(multiplier * source.Length, 1));
             for (int i = 0; i < multiplier; i++) { res.Append(source); }
 
             return res.ToString();
         }
 
         public static string PadCenterHorizontal(this string source, int totalWidth, char paddingChar = ' ')
-        { return source.PadLeft((totalWidth - source.Length) / 2 + source.Length, paddingChar).PadRight(totalWidth, paddingChar); }
+        { return string.Join('\n', source.Split('\n').Select(line => line.PadLeft((totalWidth - line.Length) / 2 + line.Length, paddingChar).PadRight(totalWidth, paddingChar))); }
 
         public static string PadCenterVertical(this string source, int totalHeight)
         {
