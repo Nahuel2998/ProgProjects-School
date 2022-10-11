43c4bf616943e17239e3e114b097460353d8e85b
NaP <nahuel2998@gmail.com>
2022-10-11 02:27:38 -0300

    Fix build script

--- PersonalExperiments/SongSwapper/win_build.ps1
+++ PersonalExperiments/SongSwapper/win_build.ps1
@@ -2,7 +2,8 @@ pp -o build/SongSwapper.exe -M Bundle::LWP -x src/SongSwapper.pl --xargs src/exa
 Copy-Item -Path src/readme.txt -Destination build
 Copy-Item -Path src/example_songs.txt -Destination build
 Copy-Item -Path src/ErrorFinder.ps1 -Destination build
-Copy-Item -Path src/SongSwapper.pl -Destination build/src/SongSwapper.pl
+mkdir build/src
+Copy-Item -Path src/SongSwapper.pl -Destination build/src
 Copy-Item -Path src/libs -Destination build -Recurse
 "[Enter to quit]"
 Read-Host;
